ea1d333f8fd3d45a78a4c4d70511713578bb5345
pbseiya <pbseiya@gmail.com>
2025-12-23 13:24:20 +0700

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,13 @@
+fn main() {
+    // นี่คือ comment
+    /*
+    นี่คือ commment แบบหลายบรรทัดที่ 1
+    นี่คือ commment แบบหลายบรรทัดที่ 2
+    นี่คือ commment แบบหลายบรรทัดที่ 3
+    */
+    println!("Hello, world! 5abc");
+
+    println!(
+        "My name is Pongsak I am {} years old {} and {}", 30, 888, 999
+    );
+}
